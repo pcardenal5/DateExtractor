5290f1d9247c1a7fa136ce5ab121fc8f29f49392
JuanOrtegax2 <202208018@alu.comillas.edu>
2023-05-15 11:46:49 +0200

    Cambios

--- Q&A.py
+++ Q&A.py
@@ -1,20 +1,18 @@
 from transformers import AutoTokenizer, RobertaForQuestionAnswering
 import torch
+def AnswerQuestions(question:str, text:str):
+    tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
+    model = RobertaForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
 
-tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
-model = RobertaForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
+    inputs = tokenizer(question, text, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
 
-question, text = "Who was Jim Henson?", "Jim Henson was a nice puppet"
+    answer_start_index = outputs.start_logits.argmax()
+    answer_end_index = outputs.end_logits.argmax()
 
-inputs = tokenizer(question, text, return_tensors="pt")
-with torch.no_grad():
-    outputs = model(**inputs)
-
-answer_start_index = outputs.start_logits.argmax()
-answer_end_index = outputs.end_logits.argmax()
-
-predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
-print(tokenizer.decode(predict_answer_tokens, skip_special_tokens=True))
+    predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
+    tokenizer.decode(predict_answer_tokens, skip_special_tokens=True)
 
 
 # # target 
