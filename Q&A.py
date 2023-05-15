3e6a22761aabf9415c3e6cd168fab1a0f6d40369
JuanOrtegax2 <202208018@alu.comillas.edu>
2023-05-15 11:18:29 +0200

    Archivos básicos

---
+++ Q&A.py
@@ -0,0 +1,26 @@
+from transformers import AutoTokenizer, RobertaForQuestionAnswering
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
+model = RobertaForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
+
+question, text = "Who was Jim Henson?", "Jim Henson was a nice puppet"
+
+inputs = tokenizer(question, text, return_tensors="pt")
+with torch.no_grad():
+    outputs = model(**inputs)
+
+answer_start_index = outputs.start_logits.argmax()
+answer_end_index = outputs.end_logits.argmax()
+
+predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
+print(tokenizer.decode(predict_answer_tokens, skip_special_tokens=True))
+
+
+# # target 
+# target_start_index = torch.tensor([14])
+# target_end_index = torch.tensor([15])
+
+# outputs = model(**inputs, start_positions=target_start_index, end_positions=target_end_index)
+# loss = outputs.loss
+# round(loss.item(), 2)
